e766b5406cdb25f164c88819a8565a4a8a8f5863
JonathanEsteves <jonathan.dev_93@outlook.com>
2025-08-01 20:26:40 -0300

    Add missing remote_backend variable after merge

--- variables.tf
+++ variables.tf
@@ -71,4 +71,20 @@ variable "vpc" {
         map_public_ip_on_launch = false
     }]
   }
-} 
+}
+
+variable "remote_backend" {
+  type = object({
+    s3_bucket                   = string
+    dynamodb_table_name         = string
+    dynamodb_table_billing_mode = string
+    dynamodb_table_hash_key     = string
+  })
+
+  default = {
+    s3_bucket                   = "workshop-s3-remote-backend-bucket-471112511203"
+    dynamodb_table_name         = "workshop-s3-state-locking-table"
+    dynamodb_table_billing_mode = "PAY_PER_REQUEST"
+    dynamodb_table_hash_key     = "LockID"
+  }
+}
